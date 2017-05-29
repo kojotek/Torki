44cdccbaeb0d427e9cca8110b23811fc1522fc27
Przemysław Nowaczyk <nowaczykp@wp.pl>
2017-05-29 21:09:39 +0200

    xcvbxcvb

--- min-torcs/run_client_best_fit.bat
+++ min-torcs/run_client_best_fit.bat
@@ -1,5 +1,5 @@
 @echo off
 type header.txt > output_fcl_best_fit.csv
-java -jar TorcsClientFcl.jar fcl\robot_best_fit.fcl gearPreferences:gears.csv >> output_fcl_best_fit.csv
+java -jar TorcsClientFcl.jar %1 gearPreferences:gears.csv >> output_fcl_best_fit.csv
 timeout 1 >nul
 @echo on
