aec0a78c65a0aec844a44e042311b2f0a15cb41d
Przemysław Nowaczyk <nowaczykp@wp.pl>
2017-07-25 12:42:28 +0200

    adfasdgasdfgdghfg

--- min-torcs/run_client_furia_genetic.bat
+++ min-torcs/run_client_furia_genetic.bat
@@ -1,4 +1,4 @@
 @echo off
-java -jar FuriaGenetic.jar furia_steer.fcl furia_accel.fcl furia_gears.fcl verbose:on gearPreferences:gears.csv racingLine:%1 trackLength:%2> output_furia_genetics.csv
+java -jar FuriaGenetic.jar furia_steer.fcl furia_accel.fcl furia_gears.fcl verbose:on gearPreferences:gears.csv racingLine:%1 trackLength:%2
 timeout 1 >nul
 @echo on
